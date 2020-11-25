0b539917683db0d3118594bc61923677a2d38fd7
Salman Javed <simply.ingenious@gmail.com>
2020-11-25 17:20:55 +0500

    added iconset app in casks list

--- mac/brew.sh
+++ mac/brew.sh
@@ -90,6 +90,7 @@ brew cask install dash
 brew cask install dropbox
 brew cask install homebrew/cask-versions/firefox-developer-edition # Firefox Dev Edition
 brew cask install google-chrome # Chrome
+brew cask install iconset
 brew cask install iina
 brew cask install iterm2
 brew cask install kap
