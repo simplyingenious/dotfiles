546a56f0159ab6a11c0fe18aad0344acd4eb8283
Salman Javed <simply.ingenious@gmail.com>
2021-08-20 14:02:19 +0500

    introduced smart image compress function

--- env/functions.sh
+++ env/functions.sh
@@ -193,3 +193,7 @@ function resetdns() {
   sudo launchctl stop homebrew.mxcl.dnsmasq
   sudo launchctl start homebrew.mxcl.dnsmasq
 }
+
+function smartcompress() {
+  mogrify -path $2 -filter Triangle -define filter:support=2 -unsharp 0.25x0.08+8.3+0.045 -dither None -posterize 136 -quality 82 -define jpeg:fancy-upsampling=off -define png:compression-filter=5 -define png:compression-level=9 -define png:compression-strategy=1 -define png:exclude-chunk=all -interlace none -colorspace sRGB $1
+}
