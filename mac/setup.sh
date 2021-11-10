32769292824cd68b303bb61eea9fd325599e9858
Salman Javed <simply.ingenious@gmail.com>
2021-11-10 20:00:51 +0500

    install script updated

--- mac/setup.sh
+++ mac/setup.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
 # Install apps and binaries with Brew
-source mac/brew.sh
+# source mac/brew.sh
 
 # Set npm permissions and install global binaries
-source shared/npm.sh
+# source shared/npm.sh
 
 # Instal zsh plugins before linking dotfiles
-antibody bundle < .zsh-plugins.txt > $HOME/.zsh-plugins.sh
+# antibody bundle < .zsh-plugins.txt > $HOME/.zsh-plugins.sh
 
 # Create symlinks for dotfiles
-source link-dotfiles.sh
+# source link-dotfiles.sh
 
 # Configure MacOS defaults.
 # You only want to run this once during setup. Additional runs may reset changes you make manually.
-source ./macos
+# source ./macos
